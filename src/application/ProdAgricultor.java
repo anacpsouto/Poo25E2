e2cde6debfe1f8e77143d0ece8eeb84e2e4cb2db
User <User@DESKTOP-5LP1QR8>
2025-06-05 17:13:29 +0100

    classe prodagricultor

--- src/application/ProdAgricultor.java
+++ src/application/ProdAgricultor.java
@@ -1,76 +1,51 @@
-/**
- * 
- */
 package application;
 
 /**
- * 
+ * Representa um produto associado a um agricultor, com preço e stock próprios.
  */
 public class ProdAgricultor {
 	private Produto produto;
 	private Double preco;
 	private int stock;
-	
-	
+
 	/**
-	 * @param produto
-	 * @param preco
-	 * @param stock
+	 * Construtor completo.
+	 * @param produto Produto de origem
+	 * @param preco Preço atribuído pelo agricultor
+	 * @param stock Quantidade disponível em stock
 	 */
-	
 	public ProdAgricultor(Produto produto, Double preco, int stock) {
 		this.produto = produto;
 		this.preco = preco;
 		this.stock = stock;
 	}
 
-
-	/**
-	 * @return the produto
-	 */
 	public Produto getProduto() {
 		return produto;
 	}
 
-
-	/**
-	 * @param produto the produto to set
-	 */
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
 
-
-	/**
-	 * @return the preco
-	 */
 	public Double getPreco() {
 		return preco;
 	}
 
-
-	/**
-	 * @param preco the preco to set
-	 */
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
 
-
-	/**
-	 * @return the stock
-	 */
 	public int getStock() {
 		return stock;
 	}
 
-
-	/**
-	 * @param stock the stock to set
-	 */
 	public void setStock(int stock) {
 		this.stock = stock;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "Produto: " + produto.getNome() + ", Preço: " + preco + "€, Stock: " + stock;
+	}
 }
