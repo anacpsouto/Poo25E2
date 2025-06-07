b6ec4812caa4019fdcf45f76c570e2b38f8b52a8
cassi <cassi@10.179.13.232>
2025-06-07 18:04:33 +0100

    adicionei o atributo precoUnitario, e os seus respetivos metodos, em conjunto com um metodo para ter o subtotal do produto.

--- src/application/ProdEncomenda.java
+++ src/application/ProdEncomenda.java
@@ -9,13 +9,15 @@ package application;
 public class ProdEncomenda {
     private int idProduto;
     private int quantidade;
+    private double precoUnitario;
 	/**
 	 * @param idProduto
 	 * @param quantidade
 	 */
-	public ProdEncomenda(int idProduto, int quantidade) {
+	public ProdEncomenda(int idProduto, int quantidade, double precoUnitario) {
 		this.idProduto = idProduto;
 		this.quantidade = quantidade;
+		this.precoUnitario = precoUnitario;
 	}
 	/**
 	 * @return the quantidade
@@ -35,6 +37,21 @@ public class ProdEncomenda {
 	public int getIdProduto() {
 		return idProduto;
 	}
+	/**
+	 * @return the precoUnitario
+	 */
+	public double getPrecoUnitario() {
+		return precoUnitario;
+	}
+	/**
+	 * @param precoUnitario the precoUnitario to set
+	 */
+	public void setPrecoUnitario(double precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+	public double getSubtotal() {
+		return quantidade * precoUnitario;
+		}
 	@Override
 	public String toString() {
 		return "ProdEncomenda [idProduto=" + idProduto + ", quantidade=" + quantidade + "]";
