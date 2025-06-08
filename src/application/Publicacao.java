f4a0b8bc3f04e1398d4c861cd1ce186bcf8b0b6b
User <User@DESKTOP-5LP1QR8>
2025-06-08 17:48:37 +0100

    classe ProdAgricultor e Publicacao

--- src/application/Publicacao.java
+++ src/application/Publicacao.java
@@ -1,11 +1,41 @@
-/**
- * 
- */
 package application;
 
 /**
- * 
+ * Representa uma publicação sobre técnicas de cultivo sustentável,
+ * criada por um agricultor.
  */
 public class Publicacao {
+	private String conteudo;
+	private Categoria categoria;
 
+	/**
+	 * Construtor da classe Publicacao.
+	 * @param conteudo Texto da publicação.
+	 * @param categoria Categoria da publicação (ex: compostagem, adubação, etc.).
+	 */
+	public Publicacao(String conteudo, Categoria categoria) {
+		this.conteudo = conteudo;
+		this.categoria = categoria;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + categoria + "] " + conteudo;
+	}
 }
